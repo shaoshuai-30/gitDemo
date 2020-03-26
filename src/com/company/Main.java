8cd610d5af3ea6d36a7b4cdbf135db8af4ff471d
shaoshuai-30 <62641909+shaoshuai-30@users.noreply.github.com>
2020-03-26 15:06:53 +0800

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,10 +2,10 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.prin哈哈tln("哈");
-        
+        System.out.println("哈哈哈");
+
         System.out.println("hjsacdncud");
-        
-        System.out.prin哈哈tln("呵呵呵");
+
+        System.out.println("咳咳咳");
     }
 }
